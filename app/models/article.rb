cdd9106b1649d24430c75b093bba18fc9089e4fd
Maksim Dimitrov <dimitrov.maksim@gmail.com>
2016-11-17 10:47:53 +0200

    More index UI tweaks

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,8 @@ class Article < ApplicationRecord
   has_many :comments, dependent: :destroy
   validates :title, presence: true, length: { minimum: 5}
   belongs_to :user
+  validates :image_url, allow_blank: true, format: {
+    with: %r{\.(gif|jpg|png)\Z}i,
+    message: 'must be a URL for GIF, JPG or PNG image.'
+  }
 end
